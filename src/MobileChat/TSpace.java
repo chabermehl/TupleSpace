06561a76a07897eaacb99ffe2cd214a3ddc68bb4
Charles <chabermehl@unm.edu>
2018-10-04 13:05:19 -0600

    initialzed required functions

--- src/MobileChat/TSpace.java
+++ src/MobileChat/TSpace.java
@@ -11,4 +11,31 @@ public class TSpace {
     public TSpace(){
         tupleSpace = new LinkedHashMap<>();
     }
+
+    /**
+     * adds a tuple to the space if it does not already exist
+     * @param key string value used to directly pull the tuple
+     * @param tuple list of object values that define the tuple
+     */
+    public void out(String key, LinkedList<Object> tuple) {
+
+    }
+
+    /**
+     * removes a tuple matching the pattern and returns it
+     * @param pattern list of object values
+     * @return tuple that matches the pattern
+     */
+    public LinkedList<Object> inp(LinkedList<Object> pattern) {
+
+    }
+
+    /**
+     * returns a tuple matching the pattern to the user
+     * @param pattern list of object values
+     * @return tuple that matches the pattern
+     */
+    public LinkedList<Object> rdp(LinkedList<Object> pattern) {
+
+    }
 }
