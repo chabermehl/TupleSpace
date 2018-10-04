d48a475751991c4504418c994e0dad2c51f8198f
Charles <chabermehl@unm.edu>
2018-10-04 12:44:08 -0600

    this should work

--- src/MobileChat/TSpace.java
+++ src/MobileChat/TSpace.java
@@ -1,11 +1,14 @@
 package MobileChat;
 
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 public class TSpace {
 
+    private Map<String, LinkedList<Object>> tupleSpace;
 
     public TSpace(){
-
+        tupleSpace = new LinkedHashMap<>();
     }
 }
