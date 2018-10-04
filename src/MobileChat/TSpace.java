df2240fcadefb1f9858460f3a9a3fdbd0bf921e0
Charles <chabermehl@unm.edu>
2018-10-04 13:12:06 -0600

    updated javadoc comments

--- src/MobileChat/TSpace.java
+++ src/MobileChat/TSpace.java
@@ -14,6 +14,7 @@ public class TSpace {
 
     /**
      * adds a tuple to the space if it does not already exist
+     * this is the worst given method name ever
      * @param key string value used to directly pull the tuple
      * @param tuple list of object values that define the tuple
      */
@@ -23,6 +24,7 @@ public class TSpace {
 
     /**
      * removes a tuple matching the pattern and returns it
+     * this method name also sucks
      * @param pattern list of object values
      * @return tuple that matches the pattern
      */
@@ -32,6 +34,7 @@ public class TSpace {
 
     /**
      * returns a tuple matching the pattern to the user
+     * this method name is just as bad
      * @param pattern list of object values
      * @return tuple that matches the pattern
      */
