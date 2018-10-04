5614c704722f1c06f71523f5c54b94903862422f
Charles <chabermehl@unm.edu>
2018-10-04 12:36:13 -0600

    realized that wasn't going to work

--- src/MobileChat/TSpace.java
+++ src/MobileChat/TSpace.java
@@ -3,11 +3,9 @@ package MobileChat;
 import java.util.LinkedList;
 
 public class TSpace {
-    private String key;
-    private LinkedList<Object> value;
 
-    public TSpace(String key, LinkedList<Object> value){
-        this.key = key;
-        this.value = value;
+
+    public TSpace(){
+
     }
 }
