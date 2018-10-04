bf3caed9913155af318afc0918f89fe0378e71cb
Charles <chabermehl@unm.edu>
2018-10-04 11:54:18 -0600

    added constructor

--- src/MobileChat/TSpace.java
+++ src/MobileChat/TSpace.java
@@ -1,4 +1,13 @@
 package MobileChat;
 
+import java.util.LinkedList;
+
 public class TSpace {
+    private String key;
+    private LinkedList<Object> value;
+
+    public TSpace(String key, LinkedList<Object> value){
+        this.key = key;
+        this.value = value;
+    }
 }
