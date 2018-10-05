2add4fa0aa4f0ce9f5bf3c25c40b0648b60d3c05
Charles <chabermehl@unm.edu>
2018-10-04 22:18:38 -0600

    add user method added

--- src/MobileChat/TextInterface.java
+++ src/MobileChat/TextInterface.java
@@ -1,10 +1,16 @@
 package MobileChat;
 
+import java.util.List;
+
 public class TextInterface {
 
     TSpace tupleSpace = new TSpace();
 
     public static void main(String args[]) {
 
+    }
+
+    private void addUser(String name, List<Object> attributes) {
+        tupleSpace.out(name, attributes);
     }
 }
