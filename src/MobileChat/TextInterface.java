1bea73c82d7900c05a7b28baac5353021ed32374
Charles <chabermehl@unm.edu>
2018-10-04 22:31:50 -0600

    created text interface instance

--- src/MobileChat/TextInterface.java
+++ src/MobileChat/TextInterface.java
@@ -1,16 +1,16 @@
 package MobileChat;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 public class TextInterface {
 
     TSpace tupleSpace = new TSpace();
+    Scanner scanner = new Scanner(System.in);
 
     public static void main(String args[]) {
-
+        TextInterface textI = new TextInterface();
     }
 
-    private void addUser(String name, List<Object> attributes) {
-        tupleSpace.out(name, attributes);
-    }
 }
